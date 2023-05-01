82bf412a9f423db38afe6cf53929f2aa07cf97e7
AlejandraDominguezCereijo <114103665+AlejandraDominguezCereijo@users.noreply.github.com>
2023-05-01 12:49:52 +0200

    Alejandra (#2)
    * Commit clases basicas Usuario
    
    * Fusion Clases Producto y clases Usuarios

---
+++ src/HistorialPedidosTotal.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class HistorialPedidosTotal {
+    public HistorialPedidosTotal() {
+    }
+
+    public HistorialPedidosTotal(ArrayList<Pedidos> totalPedidos) {
+        this.totalPedidos = totalPedidos;
+    }
+
+    private ArrayList<Pedidos> totalPedidos;
+
+    public ArrayList<Pedidos> getTotalPedidos() {
+        return totalPedidos;
+    }
+
+    public void setTotalPedidos(ArrayList<Pedidos> totalPedidos) {
+        this.totalPedidos = totalPedidos;
+    }
+}
