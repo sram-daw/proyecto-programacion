30f14d76da8d74538b16c6962fc48ced2310677c
AlejandraDominguezCereijo <114103665+AlejandraDominguezCereijo@users.noreply.github.com>
2023-05-01 12:49:52 +0200

    Alejandra (#2)
    * Commit clases basicas Usuario
    
    * Fusion Clases Producto y clases Usuarios

---
+++ src/ListaUsers.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class ListaUsers {
+
+    public ListaUsers() {
+    }
+
+    public ListaUsers(ArrayList<Cliente> listaUser) {
+        ListaUser = listaUser;
+    }
+
+    public ArrayList<Cliente> getListaUser() {
+        return ListaUser;
+    }
+
+    public void setListaUser(ArrayList<Cliente> listaUser) {
+        ListaUser = listaUser;
+    }
+
+    private ArrayList<Cliente> ListaUser;
+}
