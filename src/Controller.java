0a4cac4532ee419c1bb659caeea6d103e2c21220
AlejandraDominguezCereijo <adominguezcereijo@danielcastelao.org>
2023-05-02 13:16:51 +0200

    Pantalla de inicio Cliente comentada

--- src/Controller.java
+++ src/Controller.java
@@ -4,11 +4,14 @@ import javax.swing.*;
 
 public class Controller {
     public static void main(String[] args) {
+        //pantalla de inicio de clientes
         JFrame ventanaClientes = new InicioClientes();
+        //damos tamaño a la pantalla
         ventanaClientes.setSize(1200, 900);
+        //titulo de la pantalla
         ventanaClientes.setTitle("Inicio clientes");
         ventanaClientes.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
+        //indicamos mediante valor boolena que la pantalla sea visible
         ventanaClientes.setVisible(true);
     }
 }
